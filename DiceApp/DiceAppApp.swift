fc791a93ad3f0432435494431ea46d107358ef50
Ilya Filyaev <i.filyaev@paysenger.com>
2023-08-28 18:09:17 +0300

    Initial Commit

---
+++ DiceApp/DiceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceAppApp.swift
+//  DiceApp
+//
+//  Created by Илья Филяев on 28.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct DiceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
